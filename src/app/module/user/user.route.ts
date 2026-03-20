ef5fe4ff84201512fc34b44c34f5ce665fa8c133
dipto230 <diptoroy230305@gmail.com>
2026-03-21 01:03:32 +0530

    token jwt added

--- src/app/module/user/user.route.ts
+++ src/app/module/user/user.route.ts
@@ -1,8 +1,12 @@
 import { Router } from "express";
 import { UserController } from "./user.controller";
+import { validateRequest } from "../../middleware/validateRequest";
+import { createLawyerZodSchema } from "./user.validation";
 
 const router = Router()
 
-router.post('/create-lawyer', UserController.createLawyer)
+router.post('/create-lawyer',
+    validateRequest(createLawyerZodSchema),
+    UserController.createLawyer)
 
 export const UserRoutes = router
