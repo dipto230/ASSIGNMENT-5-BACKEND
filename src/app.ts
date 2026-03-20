9260361b812a3496e81f4e645710b2ebf4c72948
dipto230 <diptoroy230305@gmail.com>
2026-03-21 02:25:41 +0530

    route protection done

--- src/app.ts
+++ src/app.ts
@@ -4,6 +4,7 @@ import express, { Application, Request, Response } from "express";
 import { prisma } from "./app/lib/prisma";
 import { IndexRoutes } from "./app/routes";
 import { notFound } from "./app/middleware/notFound";
+import cookieParser from "cookie-parser";
 
 
 const app: Application = express();
@@ -14,6 +15,7 @@ app.use(express.urlencoded({ extended: true }));
 
 // Middleware to parse JSON bodies
 app.use(express.json());
+app.use(cookieParser())
 
 app.use("/api/v1", IndexRoutes);
 
--- src/app/module/PracticeArea/practiceArea.route.ts
+++ src/app/module/PracticeArea/practiceArea.route.ts
@@ -1,10 +1,12 @@
 import { Router } from "express";
 import { PracticeAreaController } from "./practiceArea.controller";
+import { checkAuth } from "../../middleware/checkAuth";
+import { Role } from "../../../generated/prisma/enums";
 
 const router = Router()
 
 router.post('/', PracticeAreaController.createPracticeArea)
-router.get('/', PracticeAreaController.getAllPracticeArea)
+router.get('/',checkAuth(Role.ADMIN,Role.LAWYER, Role.SUPER_ADMIN), PracticeAreaController.getAllPracticeArea)
 router.delete('/:id', PracticeAreaController.deletePracticeArea)
 
 
