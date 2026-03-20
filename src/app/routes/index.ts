f1cbb96dc63c9df7cd965abf3027468cfa4215fc
dipto230 <diptoroy230305@gmail.com>
2026-03-20 21:58:40 +0530

    route added

--- src/app/routes/index.ts
+++ src/app/routes/index.ts
@@ -1,10 +1,12 @@
 import { Router } from "express";
 import { PracticeAreaRoutes } from "../module/PracticeArea/practiceArea.route";
 import { AuthRoutes } from "../module/auth/auth.route";
+import { UserRoutes } from "../module/user/user.route";
 
 const router = Router()
-
-router.use("/practiceArea", PracticeAreaRoutes)
 router.use("/auth", AuthRoutes)
+router.use("/practiceArea", PracticeAreaRoutes)
+
+router.use("/lawyers", UserRoutes)
 
 export const IndexRoutes = router
