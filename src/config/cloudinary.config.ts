389b52206fb7b5cdfc41ff24664c49d068aeb2d4
dipto230 <diptoroy230305@gmail.com>
2026-03-22 00:30:16 +0530

    manually file upload done

--- src/config/cloudinary.config.ts
+++ src/config/cloudinary.config.ts
@@ -44,8 +44,8 @@ export const uploadFileToCloudinary = async (
         cloudinary.uploader.upload_stream(
             {
                 resource_type: "auto",
-                public_id: `ph-healthcare/${folder}/${uniqueName}`,
-                folder : `ph-healthcare/${folder}`,
+                public_id: `assignment-5/${folder}/${uniqueName}`,
+                folder : `assignment-5/${folder}`,
             },
             (error, result) => {
                 if(error){
